b2c8b104a7d2182b413852b2c1f5c7362cc192ae
root <root@ip-172-31-30-189.ap-northeast-2.compute.internal>
2018-11-12 09:36:44 +0000

    adding new file

---
+++ operators1.py
@@ -0,0 +1,32 @@
+a = 5
+b = 5.0
+
+print(id(a))
+print(id(b))
+
+print( a is b)
+print ( a ==  b)
+
+c = 10
+d = 20
+
+print(id(c))
+print(id(d))
+
+
+# membership operator
+a = "Epsilon"
+print ("sa" in a)
+
+a = 5
+a = a + 5
+a +=5
+
+print(a)
+
+a = 5
+b = 4
+
+print (a & b)
+print (a | b)
+print (help(id))
